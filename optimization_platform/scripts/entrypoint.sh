e179580a0ee5e519bac6371148ca0ec36e287f16
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2020-05-09 21:09:49 +0530

    added cert and key files

--- optimization_platform/scripts/entrypoint.sh
+++ optimization_platform/scripts/entrypoint.sh
@@ -7,9 +7,12 @@
 
 #gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT -k gevent -t $SERVICE_TIMEOUT -w $WORKER_COUNT app:app
 #export PYTHONPATH=`pwd`
-gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT -k gevent -t $SERVICE_TIMEOUT -w $WORKER_COUNT optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
+#gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT -k gevent -t $SERVICE_TIMEOUT -w $WORKER_COUNT optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
 #gunicorn --pythonpath / -b 0.0.0.0:6006 -k gevent -t 300 -w 2 optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
+gunicorn --certfile=myserver-dev.crt --keyfile=myserver-dev.key --pythonpath / -b 0.0.0.0:$SERVICE_PORT -k gevent -t $SERVICE_TIMEOUT -w $WORKER_COUNT optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
+#gunicorn --certfile=myserver-dev.crt --keyfile=myserver-dev.key --pythonpath / -b 0.0.0.0:6006 -k gevent -t 300 -w 2 optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
 
+#http --verify=myserver-dev.crt https://0.0.0.0:6006/
 # --------------------------------------------------------------------------------------------------
 # to make the container alive for indefinite time
 # --------------------------------------------------------------------------------------------------
