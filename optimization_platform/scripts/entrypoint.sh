5fe0a48575bd102beeb6e520720d192557a5a30b
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2020-05-11 15:18:12 +0530

    added nginx config

--- optimization_platform/scripts/entrypoint.sh
+++ optimization_platform/scripts/entrypoint.sh
@@ -4,13 +4,15 @@
 # generate key and cert for https using openssl
 # --------------------------------------------------------------------------------------------------
 
-openssl req -newkey rsa:2048 -nodes -keyout /myserver-dev.key -x509 -out /myserver-dev.crt -subj "/C=US/ST=TEXAS/L=AUSTIN/O=IT/CN="$HOST_PUBLIC_IP_ADDR
+#openssl req -newkey rsa:2048 -nodes -keyout /myserver-dev.key -x509 -out /myserver-dev.crt -subj "/C=US/ST=TEXAS/L=AUSTIN/O=IT/CN="$HOST_PUBLIC_IP_ADDR
 
 # --------------------------------------------------------------------------------------------------
 # start web service to provide rest end points for this container
 # --------------------------------------------------------------------------------------------------
 
-gunicorn --certfile=myserver-dev.crt --keyfile=myserver-dev.key --pythonpath / -b 0.0.0.0:$SERVICE_PORT -k gevent -t $SERVICE_TIMEOUT -w $WORKER_COUNT optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
+gunicorn --pythonpath / -b 0.0.0.0:$SERVICE_PORT -k gevent -t $SERVICE_TIMEOUT -w $WORKER_COUNT optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
+
+#gunicorn --certfile=myserver-dev.crt --keyfile=myserver-dev.key --pythonpath / -b 0.0.0.0:$SERVICE_PORT -k gevent -t $SERVICE_TIMEOUT -w $WORKER_COUNT optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
 #gunicorn --certfile=myserver-dev.crt --keyfile=myserver-dev.key --pythonpath / -b 0.0.0.0:6006 -k gevent -t 300 -w 2 optimization_platform.deployment.server:app -k uvicorn.workers.UvicornWorker
 
 
