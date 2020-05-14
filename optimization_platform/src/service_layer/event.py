3597756887b5a83b210b31c5422505f09a11236d
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2020-05-14 12:15:59 +0530

    handled None value for creation time of events

--- optimization_platform/src/service_layer/event.py
+++ optimization_platform/src/service_layer/event.py
@@ -4,11 +4,17 @@ from utils.date_utils import *
 
 
 def register_event_for_client(data_store, client_id, experiment_id, session_id, variation_id, event_name,
-                              creation_time):
-    creation_time_utc_str = utc_timestamp_to_utc_datetime_string(creation_time)
-
+                              creation_time=None):
     table = TABLE_EVENTS
-    columns_value_dict = {"client_id": client_id, "experiment_id": experiment_id,
-                          "variation_id": variation_id,
-                          "session_id": session_id, "event_name": event_name, "creation_time": creation_time_utc_str}
+
+    if creation_time is not None:
+        creation_time_utc_str = utc_timestamp_to_utc_datetime_string(creation_time)
+        columns_value_dict = {"client_id": client_id, "experiment_id": experiment_id,
+                              "variation_id": variation_id,
+                              "session_id": session_id, "event_name": event_name,
+                              "creation_time": creation_time_utc_str}
+    else:
+        columns_value_dict = {"client_id": client_id, "experiment_id": experiment_id,
+                              "variation_id": variation_id,
+                              "session_id": session_id, "event_name": event_name}
     data_store.insert_record_to_data_store(table=table, columns_value_dict=columns_value_dict)
