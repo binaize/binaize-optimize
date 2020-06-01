a0217a45f83473aaf649ef1e02378654cdeccd7a
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2020-06-01 14:20:39 +0530

    added 2 tests for server

--- tests/test_server.py
+++ tests/test_server.py
@@ -3,7 +3,9 @@ from unittest import TestCase
 import testing.postgresql
 from fastapi.testclient import TestClient
 
-pgsql = testing.postgresql.Postgresql(cache_initialized_db=True, port=55895)
+from config import AWS_RDS_PORT
+
+pgsql = testing.postgresql.Postgresql(cache_initialized_db=True, port=int(AWS_RDS_PORT))
 params = pgsql.dsn()
 from optimization_platform.deployment.server import app, logger
 
@@ -229,3 +231,118 @@ class TestServer(TestCase):
         expected_response_json = {"status": "200",
                                   "message": "Addition of Shopify Credentials for client_id test_client is successful."}
         self.assertDictEqual(d1=response_json, d2=expected_response_json)
+
+    def test_add_experiment(self):
+        self.test_sign_up_new_client()
+
+        response = client.post(
+            "/token",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            json="grant_type=password&username=test_client&password=test_password&scope=&client_id=&client_secret="
+        )
+
+        status_code = response.status_code
+        expected_status_code = 200
+        self.assertEqual(first=status_code, second=expected_status_code)
+
+        access_token = response.json()["access_token"]
+
+        response = client.post(
+            "/add_experiment",
+            headers={"Authorization": "Bearer " + access_token},
+            json={"experiment_name": "test_experiment_name",
+                  "page_type": "test_page_type",
+                  "experiment_type": "test_experiment_type",
+                  "created_on_timestamp": 1590570923,
+                  "last_updated_on_timestamp": 1590570923,
+                  "status": "test_status"}
+        )
+
+        status_code = response.status_code
+        expected_status_code = 200
+        self.assertEqual(first=status_code, second=expected_status_code)
+
+        response_json = response.json()
+        response_json.pop("experiment_id", None)
+        expected_response_json = {'experiment_name': 'test_experiment_name', 'page_type': 'test_page_type',
+                                  'experiment_type': 'test_experiment_type', 'status': 'test_status',
+                                  'client_id': 'test_client',
+                                  'creation_time': '2020-05-27 09:15:23', 'last_updation_time': '2020-05-27 09:15:23'}
+        self.assertDictEqual(d1=response_json, d2=expected_response_json)
+
+    def test_list_experiments(self):
+        self.test_sign_up_new_client()
+
+        response = client.post(
+            "/token",
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+            json="grant_type=password&username=test_client&password=test_password&scope=&client_id=&client_secret="
+        )
+
+        status_code = response.status_code
+        expected_status_code = 200
+        self.assertEqual(first=status_code, second=expected_status_code)
+
+        access_token = response.json()["access_token"]
+
+        response = client.post(
+            "/add_experiment",
+            headers={"Authorization": "Bearer " + access_token},
+            json={"experiment_name": "test_experiment_name_1",
+                  "page_type": "test_page_type_1",
+                  "experiment_type": "test_experiment_type_1",
+                  "created_on_timestamp": 1590570923,
+                  "last_updated_on_timestamp": 1590570923,
+                  "status": "test_status_1"}
+        )
+        response = client.post(
+            "/add_experiment",
+            headers={"Authorization": "Bearer " + access_token},
+            json={"experiment_name": "test_experiment_name_2",
+                  "page_type": "test_page_type_2",
+                  "experiment_type": "test_experiment_type_2",
+                  "created_on_timestamp": 1590570923,
+                  "last_updated_on_timestamp": 1590570923,
+                  "status": "test_status_2"}
+        )
+        response = client.post(
+            "/add_experiment",
+            headers={"Authorization": "Bearer " + access_token},
+            json={"experiment_name": "test_experiment_name_3",
+                  "page_type": "test_page_type_3",
+                  "experiment_type": "test_experiment_type_3",
+                  "created_on_timestamp": 1590570923,
+                  "last_updated_on_timestamp": 1590570923,
+                  "status": "test_status_3"}
+        )
+
+        response = client.get(
+            "/list_experiments",
+            headers={"Authorization": "Bearer " + access_token}
+        )
+
+        status_code = response.status_code
+        expected_status_code = 200
+        self.assertEqual(first=status_code, second=expected_status_code)
+
+        response_json = response.json()
+        for x in response_json:
+            x.pop("experiment_id", None)
+
+        expected_response_json = [{'experiment_name': 'test_experiment_name', 'page_type': 'test_page_type',
+                                   'experiment_type': 'test_experiment_type', 'status': 'test_status',
+                                   'client_id': 'test_client', 'creation_time': '27-May-2020',
+                                   'last_updation_time': '27-May-2020'},
+                                  {'experiment_name': 'test_experiment_name_1', 'page_type': 'test_page_type_1',
+                                   'experiment_type': 'test_experiment_type_1', 'status': 'test_status_1',
+                                   'client_id': 'test_client', 'creation_time': '27-May-2020',
+                                   'last_updation_time': '27-May-2020'},
+                                  {'experiment_name': 'test_experiment_name_2', 'page_type': 'test_page_type_2',
+                                   'experiment_type': 'test_experiment_type_2', 'status': 'test_status_2',
+                                   'client_id': 'test_client', 'creation_time': '27-May-2020',
+                                   'last_updation_time': '27-May-2020'},
+                                  {'experiment_name': 'test_experiment_name_3', 'page_type': 'test_page_type_3',
+                                   'experiment_type': 'test_experiment_type_3', 'status': 'test_status_3',
+                                   'client_id': 'test_client', 'creation_time': '27-May-2020',
+                                   'last_updation_time': '27-May-2020'}]
+        self.assertCountEqual(first=response_json, second=expected_response_json)
