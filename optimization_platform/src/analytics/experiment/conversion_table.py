0044e732f85b16fa513cd104cfacff04544f9dd0
Tuhin Sharma <tuhinsharma@Tuhins-MacBook-Pro.local>
2020-07-06 21:04:27 +0530

    added avg order value

--- optimization_platform/src/analytics/experiment/conversion_table.py
+++ optimization_platform/src/analytics/experiment/conversion_table.py
@@ -5,16 +5,20 @@ def get_sales_conversion_df(client_id, data_store, experiment_id):
     sql = \
         """ select distinct
                 cookie_order_table.session_id,
+                cookie_order_table.order_id,
                 events_table.variation_id,
                 cookie_order_table.cart_token,
                 cookie_order_table.variant_id,
-                cookie_order_table.variant_quantity
+                cookie_order_table.variant_quantity,
+                cookie_order_table.variant_price
             from
             (select
                     table1.session_id,
                     table2.cart_token,
                     table2.variant_id,
-                    table2.variant_quantity
+                    table2.variant_quantity,
+                    table2.order_id,
+                    table2.variant_price
                 from
                     (
                     select
@@ -26,17 +30,20 @@ def get_sales_conversion_df(client_id, data_store, experiment_id):
                         client_id = '{client_id}'
                     )
                     table1
-                    inner join
+                    right join
                         (
                         select
                             variant_id,
                             variant_quantity,
-                            cart_token
+                            cart_token,
+                            order_id,
+                            variant_price
                         from
                             orders
                         where
                             client_id = '{client_id}'
                             and payment_status = True
+                            and updated_at > '2020-06-25 08:55:48+00'
                         )
                         table2
                         on (table1.cart_token = table2.cart_token)) cookie_order_table
@@ -55,8 +62,8 @@ def get_sales_conversion_df(client_id, data_store, experiment_id):
     sales_conv_df = None
     if mobile_records is not None and len(mobile_records) > 0:
         sales_conv_df = pd.DataFrame.from_records(mobile_records)
-        sales_conv_df.columns = ["session_id", "variation_id", "cart_token", "variant_id",
-                                 "variant_quantity"]
+        sales_conv_df.columns = ["session_id", "order_id", "variation_id", "cart_token", "variant_id",
+                                 "variant_quantity", "variant_price"]
     return sales_conv_df
 
 
@@ -157,8 +164,11 @@ def get_conversion_table_of_experiment(data_store, client_id, experiment_id):
     if sales_conv_df is None:
         df["sales_conversion_count"] = 0
     else:
+        sales_conv_df["variant_total_price"] = sales_conv_df["variant_quantity"] * sales_conv_df["variant_price"]
         sales_conv_df = sales_conv_df.groupby('variation_id').agg({
-            'session_id': [('sales_conversion_count', lambda x: len(set(x)))]
+            'session_id': [('sales_conversion_count', lambda x: len(set(x)))],
+            'order_id': [('order_count', lambda x: len(set(x)))],
+            'variant_total_price': [('total_order_value', lambda x: sum(x))]
         })
         sales_conv_df.columns = sales_conv_df.columns.droplevel()
         sales_conv_df["variation_id"] = sales_conv_df.index
@@ -167,11 +177,15 @@ def get_conversion_table_of_experiment(data_store, client_id, experiment_id):
                       how='left', left_on=["variation_id"],
                       right_on=["variation_id"])
         df["sales_conversion_count"] = df["sales_conversion_count"].fillna(0).astype(int)
+        df["order_count"] = df["order_count"].fillna(0).astype(int)
+        df["total_order_value"] = df["total_order_value"].fillna(0).astype(float)
 
     df["goal_conversion"] = df["goal_conversion_count"] * 100 / (df["num_visitor"] + 0.01)
     df["goal_conversion"] = df["goal_conversion"].map(lambda x: min(100.00, round(x, 2)))
     df["sales_conversion"] = df["sales_conversion_count"] * 100 / (df["num_visitor"] + 0.01)
     df["sales_conversion"] = df["sales_conversion"].map(lambda x: min(100.00, round(x, 2)))
+    df["avg_total_order_value"] = df["total_order_value"] / (df["order_count"] + 0.01)
+    df["avg_total_order_value"] = df["avg_total_order_value"].map(lambda x: round(x, 2))
 
     result = df.to_dict(orient="records")
     return result
